51e20c1d29f318e816af6192fe34545f508e7fd4
Dennis Guiboche <dguiboche2@academic.rrc.ca>
2026-01-16 09:49:55 -0600

    added more shows

--- src/components/showlist/ShowList.tsx
+++ src/components/showlist/ShowList.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from 'react'; 
 
-export const ShowList: React.FC = () => {   
+export const ShowList: React.FC = () => {   // react functional component
     const shows = [
         { id: 1, title: 'Better Call Saul', genre: 'Drama' },
         { id: 2, title: 'Stranger Things', genre: 'Sci-Fi' },
@@ -8,6 +8,10 @@ export const ShowList: React.FC = () => {
         { id: 4, title: 'Game of Thrones', genre: 'Drama' },
         { id: 5, title: 'The Simpsons', genre: 'Comedy' },
         { id: 6, title: 'The Walking Dead', genre: 'Horror' },
+        { id: 7, title: 'Fallout', genre: 'Sci-fi'},
+        { id: 8, title: "Dexter", genre: "Crime Thriller"},
+        { id: 9, title: "Spongebob Squarepants", genre: "Comedy"},
+        { id: 10, title: "My Hero Academia", genre: "Action"},
         
     ];
 
@@ -15,7 +19,7 @@ export const ShowList: React.FC = () => {
         <section className="show-list">
             <h2>List of shows</h2>
             <ul>
-                {shows.map(show => (
+                {shows.map(show => ( // loops over array to create list items
                     <li key={show.id}>
                         <strong>{show.title}</strong> - {show.genre}
                     </li>
