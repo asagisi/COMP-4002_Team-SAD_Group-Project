a443ad8ea40319edf3011cc2fa7303a5cd2e868b
asagisi <asagisi@academic.rrc.ca>
2026-01-16 08:34:15 -0600

    added exported user progress list to WatchProgress component

--- src/components/watchprogress/WatchProgress.tsx
+++ src/components/watchprogress/WatchProgress.tsx
@@ -8,3 +8,45 @@ type WatchProgress = {
   totalSeasons: number;
   status: "Not Started" | "Watching" | "Finished";
 };
+
+export const WatchProgress: React.FC = () => {   
+    const progress = [
+        {
+          id: 1,
+          title: "South Park",
+          currentSeason: 1,
+          currentEpisode: 1,
+          totalSeasons: 28,
+          status: "Not Started",
+        },
+        {
+          id: 2,
+          title: "Dr. House",
+          currentSeason: 4,
+          currentEpisode: 9,
+          totalSeasons: 8,
+          status: "Watching",
+        },
+        {
+          id: 3,
+          title: "Dragon Ball Z",
+          currentSeason: 9,
+          currentEpisode: 38,
+          totalSeasons: 9,
+          status: "Finished",
+        },
+    ];
+
+    return (
+        <section className="watch-progress">
+            <h2>My Watch Progress</h2>
+            <ul>
+                {progress.map(progress => (
+                    <li key={progress.id}>
+                        {progress.title} - {progress.status} (S{progress.currentSeason}:Ep{progress.currentEpisode}\S{progress.totalSeasons})
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
+};
