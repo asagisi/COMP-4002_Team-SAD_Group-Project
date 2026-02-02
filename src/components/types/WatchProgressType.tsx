753ab6f54d88c2904efd1a91777f00731dd5075d
asagisi <asagisi@academic.rrc.ca>
2026-02-01 23:34:43 -0600

    Added notes for watchprogress

--- src/components/types/WatchProgressType.tsx
+++ src/components/types/WatchProgressType.tsx
@@ -6,3 +6,12 @@ export type WatchProgress = {
   totalSeasons: number;
   status: "Not Started" | "Watching" | "Finished";
 };
+
+// need to use title from shows.tsx for title in WatchProgressType.tsx
+// affected files:
+// WatchProgress.tsx
+// WatchProgressAdd.tsx
+// WatchProgressEdit.tsx
+// WatchProgressDelete.tsx
+// WatchProgressList.tsx
+// MyShows.tsx
