214b372bd57bb923f2c9b4a492c1c9c629601457
Dennis Guiboche <dguiboche2@academic.rrc.ca>
2026-02-01 21:39:54 -0600

    updated the feature; gave the form its own component file and that file takes in props; also added the elemental removal/addition functionality

---
+++ src/components/showlist/ShowSearchForm.tsx
@@ -0,0 +1,34 @@
+import { type Show } from '../data/shows';
+
+type ShowSearchFormProps = {
+  searchShow: string;
+  setSearchShow: (value: string) => void;
+  filteredShow: Show[];
+  toggleHide: (id: number) => void;
+};
+
+export const ShowSearchForm: React.FC<ShowSearchFormProps> = ({
+  searchShow,
+  setSearchShow,
+  filteredShow,
+  toggleHide,
+}) => {
+  return (
+    <div className="show-search">
+      <input
+        type="text"
+        placeholder="Search shows..."
+        value={searchShow}
+        onChange={(e) => setSearchShow(e.target.value)}
+      />
+      <ul>
+        {filteredShow.map((show) => (
+          <li key={show.id}>
+            <strong>{show.title}</strong> - {show.genre} ({show.year})
+            <button onClick={() => toggleHide(show.id)}>Hide show</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
