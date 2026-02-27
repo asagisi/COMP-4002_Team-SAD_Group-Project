02ee9385c798b5bc7d525e9b5ccde084874a1f89
asagisi <asagisi@academic.rrc.ca>
2026-02-27 08:58:00 -0600

    Updated WatchProgress types and add place holder data

--- src/components/types/WatchProgressType.tsx
+++ src/components/types/WatchProgressType.tsx
@@ -1,17 +1,9 @@
 export type WatchProgress = {
   id: number;
   title: string;
+  showId: number;
   currentSeason: number;
   currentEpisode: number;
   totalSeasons: number;
   status: "Not Started" | "Watching" | "Finished";
 };
-
-// need to use title from shows.tsx for title in WatchProgressType.tsx
-// affected files:
-// WatchProgress.tsx
-// WatchProgressAdd.tsx
-// WatchProgressEdit.tsx
-// WatchProgressDelete.tsx
-// WatchProgressList.tsx
-// MyShows.tsx
