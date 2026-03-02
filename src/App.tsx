7ead75fd345b02cb74c8cb09dc1d16cf1073dec8
Dennis Guiboche <dguiboche2@academic.rrc.ca>
2026-03-01 22:01:28 -0800

    refactored; app.tsx doesnt hold the shared state anymore, header is cleaner with no interface of headerprops of favourite show, and favourite show references the hook now

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css'
-import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Header } from './components/header/header'
 import { Home } from './pages/Home'
@@ -7,19 +6,15 @@ import { ShowListPage } from './pages/ShowListPage'
 import { MyShowsPage } from './pages/MyShows'
 import { WatchProgressPage } from './pages/WatchProgressPage'
 import { Footer } from './components/footer/footer'
-import { type Show } from './types/Show'
 
 
 
 
 function App() {
-  // this is the shared state for the favourite show
-  const [currentFavourite, setCurrentFavourite] = useState<Show | null>(null)
-
   return (
     <Router>
       <div className="App">
-        <Header currentFavourite={currentFavourite} setCurrentFavourite={setCurrentFavourite} />
+        <Header />
         <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
