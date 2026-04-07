69d2678a77f6bab867c6ad7c556b6dacf94cb502
asagisi <asagisi@academic.rrc.ca>
2026-04-07 13:39:00 -0500

    Restored type interface

--- shared/types/WatchProgressType.tsx
+++ shared/types/WatchProgressType.tsx
@@ -1,6 +1,8 @@
 export type WatchProgress = {
   id: number;
+  title: string;
   showId: number;
   currentEpisode: number;
+  totalEpisodes: number;
   status: "Not Started" | "Watching" | "Finished";
 };
