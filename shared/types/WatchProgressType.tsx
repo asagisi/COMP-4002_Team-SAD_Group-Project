8a213e17862988130b271798ad088414835662d2
asagisi <asagisi@academic.rrc.ca>
2026-04-07 13:21:02 -0500

    Moved types to root shared folder

--- shared/types/WatchProgressType.tsx
+++ shared/types/WatchProgressType.tsx
@@ -1,8 +1,6 @@
 export type WatchProgress = {
   id: number;
-  title: string;
   showId: number;
   currentEpisode: number;
-  totalEpisodes: number;
   status: "Not Started" | "Watching" | "Finished";
 };
