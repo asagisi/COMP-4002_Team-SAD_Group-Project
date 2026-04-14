5ebeb31525ead8236339cda78dc176b3f2120c1d
Dennis Guiboche <dguiboche2@academic.rrc.ca>
2026-04-14 13:27:18 -0500

    fixed pathing issues

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Header } from './components/header/header'
-import { Home } from './pages/Home'
-import { ShowListPage } from './pages/ShowListPage'
-import { MyShowsPage } from './pages/MyShows'
-import { WatchProgressPage } from './pages/WatchProgressPage'
-import { ProfilePage } from './pages/ProfilePage'
-import { Footer } from './components/footer/footer'
+import { Header } from './layout/header/header'
+import { Home } from './layout/pages/Home'
+import { ShowListPage } from './layout/pages/ShowListPage'
+import { MyShowsPage } from './layout/pages/MyShows'
+import { WatchProgressPage } from './layout/pages/WatchProgressPage'
+import { ProfilePage } from './layout/pages/ProfilePage'
+import { Footer } from './layout/footer/footer'
 
 
 
